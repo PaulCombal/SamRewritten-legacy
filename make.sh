f2109c83ee7644b4ca2f2e8e766d8e920ea30a8e
telans <telans@protonmail.com>
2019-10-20 23:03:46 +1300

    use lowercase executable names

--- make.sh
+++ make.sh
@@ -7,6 +7,6 @@ g++ -std=c++17 -g \
     $(pkg-config --cflags --libs gtk+-3.0) -rdynamic -export-dynamic -pthread -Wall -lpthread -lgmodule-2.0 -lsteam_api -lcurl -lyajl -ldl \
     ${SRC_FILES} \
     -L${MAKE_PATH}/bin \
-    -o ${MAKE_PATH}/bin/SamRewritten \
+    -o ${MAKE_PATH}/bin/samrewritten \
 
 echo "If there weren't any compilation errors, you can launch SamRewritten with ./bin/launch.sh"
